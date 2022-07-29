e2eb88cc518d53cb2a26c1e2cb6336f1ca30df6a
Benjamin Michaelis <benjamin@michaelis.net>
2022-07-28 19:09:02 -0700

    Update main_undergraduateresearchportal-app.yml

--- UndergraduateResearchPortal.Web/src/views/CoalesceExample.vue
+++ UndergraduateResearchPortal.Web/src/views/CoalesceExample.vue
@@ -40,7 +40,7 @@ export default class CoalesceExample extends Vue {
   private user = new UserViewModel();
 
   async created() {
-    await this.user.$load(1);
+    await this.user.$load();
     this.user.$startAutoSave(this, { wait: 500, debounce: { maxWait: 3000 } });
   }
 }
